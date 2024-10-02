ee91b6eb3666e6ddd0d8de8baeeca1d041b65960
Arvid Nilsson <arvid2.nilsson@gmail.com>
2024-10-02 16:43:43 +0200

    CRT Warp with Moire

--- examples/example_glfw_opengl3/main.cpp
+++ examples/example_glfw_opengl3/main.cpp
@@ -8,9 +8,9 @@
 // - Introduction, links and more at the top of imgui.cpp
 
 #include "imgui.h"
-#include "implot.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "implot.h"
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
@@ -19,6 +19,8 @@
 #include <GL/glew.h>
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#include <vector>
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
@@ -31,6 +33,8 @@
 #ifdef __EMSCRIPTEN__
 #include "../libs/emscripten/emscripten_mainloop_stub.h"
 #endif
+
+//#define DISABLE_CRT_CURVATURE
 
 int upscale_x = 3;
 int upscale_y = 6;
@@ -47,7 +51,7 @@ int main(int, char**)
     if (!glfwInit())
         return 1;
 
-    // Decide GL+GLSL versions
+        // Decide GL+GLSL versions
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     // GL ES 2.0 + GLSL 100
     const char* glsl_version = "#version 100";
@@ -59,24 +63,24 @@ int main(int, char**)
     const char* glsl_version = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on Mac
 #else
     // GL 3.0 + GLSL 130
     const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
     // We want to render ImGUI content to an offscreen FBO of size 320x240.
     // Then we draw the contents of the FBO to screen using a CRT shader. We need at least 4x upscale here
-    //int fbo_size[2] = { 320, 240 };
-    //int upscale = 4;
+    // int fbo_size[2] = { 320, 240 };
+    // int upscale = 4;
     // We need an upscale of a multiple of 3 to get the correct CRT shader effect
-    int fbo_size[2] = { 640, 240 };
-    int window_size[2] = { fbo_size[0] * upscale_x, fbo_size[1] * upscale_y };
+    int fbo_size[2] = {640, 240};
+    int window_size[2] = {fbo_size[0] * upscale_x, fbo_size[1] * upscale_y};
     bool use_crt_shader = true;
 
     // Create window with graphics context
@@ -92,13 +96,14 @@ int main(int, char**)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImPlot::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
+    // ImGui::StyleColorsLight();
     float scale = use_crt_shader ? 1.0f : upscale_x;
     ImGui::GetStyle().ScaleAllSizes(scale); // Scale UI
 
@@ -118,14 +123,14 @@ int main(int, char**)
     // - Read 'docs/FONTS.md' for more instructions and details.
     // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
     // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("Topaz_a1200_v1.0.ttf", 16.0f * scale);
+    // io.Fonts->AddFontDefault();
+    // io.Fonts->AddFontFromFileTTF("Topaz_a1200_v1.0.ttf", 16.0f * scale);
     io.Fonts->AddFontFromFileTTF("Topaz_a500_v1.0.ttf", 16.0f * scale);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+    // IM_ASSERT(font != nullptr);
 
     // Our state
     bool show_demo_window = true;
@@ -150,6 +155,28 @@ int main(int, char**)
         // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
 
+        // If GLFW reports that key F11 was pressed, toggle fullscreen
+        static bool toggle = false;
+        if (!toggle && glfwGetKey(window, GLFW_KEY_F11) == GLFW_PRESS) {
+            toggle = true;
+
+            static int w = window_size[0], h = window_size[1];
+            if (glfwGetWindowMonitor(window)) {
+                glfwSetWindowMonitor(window, nullptr, 100, 100, w, h, 0);
+                glfwSetWindowSize(window, w, h);
+            } else {
+                // Get the size when windowed
+                glfwGetWindowSize(window, &w, &h);
+                GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+                const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+                glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+            }
+        }
+
+        if (glfwGetKey(window, GLFW_KEY_F11) == GLFW_RELEASE) {
+            toggle = false;
+        }
+
         // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
@@ -167,16 +194,16 @@ int main(int, char**)
             static float f = 0.0f;
             static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Text("This is some useful text."); // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
             ImGui::Checkbox("Another Window", &show_another_window);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
                 counter++;
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
@@ -186,9 +213,8 @@ int main(int, char**)
         }
 
         // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        if (show_another_window) {
+            ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
             ImGui::Text("Hello from another window!");
             if (ImGui::Button("Close Me"))
                 show_another_window = false;
@@ -203,7 +229,7 @@ int main(int, char**)
             static GLuint crt_texture = 0;
             static GLuint crt_vao = 0;
             static GLuint crt_vbo = 0;
-            static GLuint locations[4] = { 0, 0, 0, 0 };
+            static GLuint locations[4] = {0, 0, 0, 0};
             if (!fbo) {
                 glGenFramebuffers(1, &fbo);
                 glGenTextures(1, &fbo_texture);
@@ -228,7 +254,17 @@ int main(int, char**)
                     out vec2 uv;
                     void main() {
                         uv = position * 0.5 + 0.5; // + vec2(0.5/width, 0.5/height);
-                        gl_Position = vec4(position, 0.0, 1.0);
+                        // Warp the gl_Position to get the CRT curvature effect
+                        vec2 delta = uv - 0.5;
+                        float delta2 = dot(delta.xy, delta.xy);
+                        float delta4 = delta2 * delta2;
+                        float warp_factor = 0.25;
+                        float delta_offset = delta4 * warp_factor;
+
+                        vec2 tmp = uv - delta * delta_offset;
+                        gl_Position = vec4(tmp * 2.0 - 1.0, 0.0, 1.0);
+                        //gl_Position = vec4(position, 0.0, 1.0);
+                        //uv = vec2(0.999, 0.999);
                     }
                 )";
                 glShaderSource(vs, 1, &vs_src, NULL);
@@ -297,8 +333,11 @@ int main(int, char**)
                     }
 
                     void main() {
+                        // Debug uv coords
+                        //frag_color = vec4(uv, 0.0, 1.0);
                         // Fractional pixel value
-                        //vec2 fpx = gl_FragCoord.xy;
+                        //vec2 px = gl_FragCoord.xy;
+                        //frag_color = texture(tex, px / vec2(width, height));
                         // The problem with gl_FragCoord is it's not interpolated.
                         // We need fractional pixel values.
                         // We can get that by passing the position from the vertex shader
@@ -343,9 +382,16 @@ int main(int, char**)
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
             glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+            // Prepare the window surface
             int display_w, display_h;
             glfwGetFramebufferSize(window, &display_w, &display_h);
-            glViewport(0, 0, display_w, display_h);
+            // Center the FBO in the window and scale proportionally to fit the window
+            float s = std::min((float)display_w / window_size[0], (float)display_h / window_size[1]);
+            int x = (display_w - s*window_size[0]) / 2;
+            int y = (display_h - s*window_size[1]) / 2;
+            glViewport(x, y, s*window_size[0], s*window_size[1]);
+            glClearColor(0, 0, 0, 1);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
             // Render the FBO to the window using the CRT shader
             if (!crt_texture) {
@@ -363,34 +409,97 @@ int main(int, char**)
             glUniform1f(locations[2], fbo_size[1]);
             static float scanline = 0;
             ++scanline;
-            if (scanline == fbo_size[1]) scanline = 0;
+            if (scanline == fbo_size[1])
+                scanline = 0;
             glUniform1f(locations[3], scanline);
+#ifndef DISABLE_CRT_CURVATURE
+            static int num_triangles = 0;
+#endif
             if (!crt_vao) {
+#ifdef DISABLE_CRT_CURVATURE
                 glGenVertexArrays(1, &crt_vao);
                 glBindVertexArray(crt_vao);
                 glGenBuffers(1, &crt_vbo);
                 glBindBuffer(GL_ARRAY_BUFFER, crt_vbo);
                 float vertices[] = {
-                    -1.0f, -1.0f,
-                     1.0f, -1.0f,
-                     1.0f,  1.0f,
-                    -1.0f,  1.0f,
+                    -1.0f,
+                    -1.0f,
+                    1.0f,
+                    -1.0f,
+                    1.0f,
+                    1.0f,
+                    -1.0f,
+                    1.0f,
                 };
                 glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
                 glEnableVertexAttribArray(0);
                 glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+#else
+                // Generate a mesh of 20x20 pixel big tiles that will be used to apply the CRT shader
+                // the mesh will be deformed to emulate CRT curvature
+                glGenVertexArrays(1, &crt_vao);
+                glBindVertexArray(crt_vao);
+                glGenBuffers(1, &crt_vbo);
+                glBindBuffer(GL_ARRAY_BUFFER, crt_vbo);
+
+                int numTilesX = window_size[0] / 20; // 20x20 pixel tiles
+                int numTilesY = window_size[1] / 20;
+                float tileWidth = 2.0f / numTilesX;
+                float tileHeight = 2.0f / numTilesY;
+
+                std::vector<float> vertices;
+                for (int y = 0; y < numTilesY; y++) {
+                    for (int x = 0; x < numTilesX; x++) {
+                        float startX = -1.0f + x * tileWidth;
+                        float startY = -1.0f + y * tileHeight;
+                        float endX = startX + tileWidth;
+                        float endY = startY + tileHeight;
+
+                        // Triangle 1
+                        vertices.push_back(startX);
+                        vertices.push_back(startY);
+                        vertices.push_back(endX);
+                        vertices.push_back(startY);
+                        vertices.push_back(endX);
+                        vertices.push_back(endY);
+
+                        // Triangle 2
+                        vertices.push_back(startX);
+                        vertices.push_back(startY);
+                        vertices.push_back(endX);
+                        vertices.push_back(endY);
+                        vertices.push_back(startX);
+                        vertices.push_back(endY);
+                    }
+                }
+                assert(vertices.size() == numTilesX * numTilesY * 6 * 2);
+                num_triangles = vertices.size() / 6;
+                glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+                glEnableVertexAttribArray(0);
+                glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+#endif
+                // See if we got any GL errors in the queue
+                GLenum err;
+                while ((err = glGetError()) != GL_NO_ERROR) {
+                    fprintf(stderr, "GL error: %d\n", err);
+                }
             }
+
             glBindVertexArray(crt_vao);
 
+#ifdef DISABLE_CRT_CURVATURE
             glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-
+#else
+            //glDrawArrays(/*GL_TRIANGLES*/ GL_LINE_STRIP, 0, num_triangles * 3);
+            glDrawArrays(GL_TRIANGLES, 0, num_triangles * 3);
+#endif
         } else {
             ImGui::Render();
             int display_w, display_h;
             glfwGetFramebufferSize(window, &display_w, &display_h);
             glViewport(0, 0, display_w, display_h);
             glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         }
 
